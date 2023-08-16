5739238fd91f2e39f1bb3e279de172d1399a2d4e
Alfredo <icealfredo123@gmail.com>
2023-08-15 20:31:19 -0500

    Made camera controls dir and testcub dir

---
+++ components/controls/CameraControls.tsx
@@ -0,0 +1,18 @@
+import { OrbitControls } from '@react-three/drei'
+
+export function CustomCameraControls(){
+    return (
+        orbitContorlsCall()
+    )
+}
+
+
+function orbitContorlsCall(){
+    return (
+        <>
+            <OrbitControls enableDamping = { false } 
+             position={[ 0, 5, 0]}
+            />
+        </>
+    )
+}
---
+++ components/objects/testCube.tsx
@@ -0,0 +1,21 @@
+import { ThreeElements } from '@react-three/fiber'
+import { useRef, useState } from 'react';
+
+export function TestBox(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
