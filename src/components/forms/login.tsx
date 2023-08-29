2c187496f79be96b9d4c979d1f2a9f181a65aeb6
CRAKZOR <nj4u26@aol.com>
2023-08-29 15:46:27 -0500

    loginForm updates

--- src/components/forms/login.tsx
+++ src/components/forms/login.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { FormEventHandler, useReducer } from 'react';
 
 type State = {
   email:    string;
@@ -9,7 +9,7 @@ type State = {
 type Action = 
     | { type: 'SET_EMAIL',          payload: string }
     | { type: 'SET_PASSWORD',       payload: string }
-    | { type: 'VALIDATE_FIELDS',    payload: string };
+    | { type: 'VALIDATE_FIELDS' };
 
 const initialState : State = {
     email:      '',
@@ -35,85 +35,58 @@ export const LoginForm: React.FC = () => {
 
     const [ state, dispatch ] = useReducer(reducer, initialState );
 
-<<<<<<< HEAD
-    const handleSubmit = ( e:Event ) => {
+    const handleSubmit = ( e:React.FormEvent<HTMLFormElement> ) => {
         e.preventDefault();
 
-        dispatch({ type: 'VALIDATE_FIELDS' })
+        dispatch({ type: 'VALIDATE_FIELDS', })
 
         if (!state.errorMsg) {
             // handle login
             console.log(state);
         }
     }
-=======
-    const handleSubmit = (e:Event) => 
->>>>>>> main
-
-    const { register, handleSubmit, errors, watch } = useForm<State>();
-    const password = useRef({});
-    password.current = watch("password", "");
-
-    const onSubmit: SubmitHandler<State> = (data) => {
-        // Handle login logic here
-        console.log(data);
-    };
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="p-6 space-y-8 bg-white w-96 rounded-md shadow-md">
                 <h1 className="text-2xl font-semibold">Login</h1>
-
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-                <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                    Email Address
-                    </label>
-                    <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    ref={register({
-                        required: "Email is required",
-                        pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                        message: "Invalid email address"
-                        }
-                    })}
-                    />
-                    {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
-                </div>
-
-                <div>
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password
-                    </label>
-                    <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    className="mt-1 p-2 w-full border rounded-md"
-                    ref={register({
-                        required: "Password is required",
-                        minLength: {
-                        value: 8,
-                        message: "Password must have at least 8 characters"
-                        }
-                    })}
-                    />
-                    {errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
-                </div>
-
-                <div>
-                    <button
-                    type="submit"
-                    className="w-full p-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                    >
-                    Log in
-                    </button>
-                </div>
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div>
+                        <label htmlFor="form-email" className="block text-sm font-medium text-gray-700">
+                            {"Email Address"}
+                        </label>
+                        <input
+                            id          = "form-email"
+                            name        = "email"
+                            type        = "email"
+                            value       = {state.email}
+                            onChange    = {e => dispatch({ type: 'SET_EMAIL', payload: e.target.value })}
+                            className   = "mt-1 p-2 w-full border rounded-md"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="form-password" className="block text-sm font-medium text-gray-700">
+                            {"Password"}
+                        </label>
+                        <input
+                            id          = "form-password"
+                            name        = "password"
+                            type        = "password"
+                            value       = {state.password}
+                            onChange    = {e => dispatch({ type: 'SET_PASSWORD', payload: e.target.value })}
+                            className   = "mt-1 p-2 w-full border rounded-md"
+                        />
+                    </div>
+                    <div>
+                        <button
+                        type="submit"
+                        className="w-full p-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                        >
+                            {"Log in"}
+                        </button>
+                    </div>
                 </form>
+                {state.errorMsg && <p className="text-red-500 text-xs">{state.errorMsg}</p>}
             </div>
         </div>
     );
