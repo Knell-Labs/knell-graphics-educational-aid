1808b3f3ef679f50adbf212106a9d8cbb90a4728
CRAKZOR <nj4u26@aol.com>
2023-08-30 10:56:25 -0500

    modal adjustments, welcome popup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,27 +10,20 @@ import Button from '../components/ui/button';
 import { ThreeScene, AnimeExample, ReactFiber  } from "../components/test";
 
 const welcomeBlock = (() => (<div>
-  <p>
-  {`
-    🔹 Dive Right Back In!
-    Login to pick up where you left off and access your saved 3D creations.
-
-    🔹 Just Browsing?
-    Continue as a guest and explore our powerful 3D modeling tools. Remember, any progress won't be saved.
-
-    🔹 Collaborate in Real-Time!
-    Create, share, and collaborate on your 3D projects by sharing unique links with fellow creators.
-
-    Happy Modeling! 🛠🌐
-
-    `.replace(/\n\n+g/, '\n')
-  }
+  <p className="my-2">
+    <span className="font-medium text-lg">{"🔹 Dive Right Back In!\n"}</span>
+    {"Login to pick up where you left off and access your saved 3D creations.\n\n"}
+    <span className="font-medium text-lg">{"🔹 Just Browsing?\n"}</span>
+    {"Continue as a guest and explore our powerful 3D modeling tools. Remember, any progress won't be saved.\n\n"}
+    <span className="font-medium text-lg">{"🔹 Collaborate in Real-Time!\n"}</span>
+    {"Create, share, and collaborate on your 3D projects by sharing unique links with fellow creators.\n\n"}
+    <span className="font-bold text-xl">{"Happy Modeling! 🛠🌐"}</span>
   </p>
-  <div className="w-full d-flex justify-center">
-    <Button variant="primary" onClick={() => console.log('Clicked!')}>
+  <div className="w-full flex justify-around gap-2">
+    <Button variant="primary" fullWidth onClick={() => console.log('Clicked!')}>
       Login
     </Button>
-    <Button variant="secondary" onClick={() => console.log('Clicked!')}>
+    <Button variant="secondary" fullWidth onClick={() => console.log('Clicked!')}>
       Continue as Guest
     </Button>
   </div>
@@ -38,7 +31,16 @@ const welcomeBlock = (() => (<div>
 
 export default function Home() {
 
-    const [ modalBody, setModalBody ] = useState<React.ReactElement | null>(welcomeBlock);
+    const [ modalBody, setModalBody ] = useState<React.ReactElement | null>(null);
+    const [ showModal, setShowModal ] = useState<boolean>(false);
+
+    useEffect(() => {
+      setModalBody(welcomeBlock)
+    }, [])
+    useEffect(() => {
+      setShowModal(!!modalBody)
+    }, [modalBody])
+
     const [ isWebGLAvailable, setIsWebGLAvailable ] = useState(false);  
 
     useEffect(() => {
@@ -49,15 +51,14 @@ export default function Home() {
   return (
     <>
 
-        { modalBody && (
-          <Modal
-            title   = "Welcome to Knell Labs"
-            isOpen  = { modalBody && true }
-            onClose = { () => setModalBody(null) }
-          >
-            {modalBody}
-          </Modal>
-        )}
+        
+        <Modal
+          title   = "Welcome to Knell Labs"
+          isOpen  = { showModal }
+          onClose = { () => setShowModal(false) }
+        >
+          {modalBody}
+        </Modal>
         
         <div className="fixed top-0 left-0 w-full h-full overflow-hidden items-center justify-center">
             <BasicScene/>
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,35 +1,39 @@
 import React, { FC, ReactNode, useEffect } from 'react';
 
 interface ModalProps {
-  title?: string;
-  isOpen: boolean;
-  onClose: () => void;
-  children?: ReactNode;
+  title?:     string;
+  isOpen:     boolean;
+  onClose:    () => void;
+  children?:  ReactNode;
 }
 
 const Modal: FC<ModalProps> = ({ title, isOpen, onClose, children }) => {
 
-    useEffect(() => {
-      const handleEscape = (event: KeyboardEvent) => {
-        if (event.key === 'Escape') onClose();
-      };
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') onClose();
+    };
 
-      document.addEventListener('keydown', handleEscape);
-      return () => {
-        document.removeEventListener('keydown', handleEscape);
-      };
-    }, [onClose]);
+    document.addEventListener('keydown', handleEscape);
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [onClose]);
+
+  const visibilityClass = isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none';
 
   return (
-    <div className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <div className="fixed inset-0 bg-black opacity-50 transition-opacity duration-300">{title}</div>
-      <div className="text-black bg-white p-8 w-1/2 rounded-lg shadow-lg relative z-10 transition-transform transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-4 scale-95'}">
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-lg font-bold"
-        >
-          &times;
-        </button>
+    <div className={`text-black fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${visibilityClass}`}>
+      <div className="fixed inset-0 bg-black opacity-50 transition-opacity duration-500"></div>
+      <div className={`bg-white p-8 w-1/2 rounded-lg shadow-lg relative z-10 transition-transform transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-4 scale-95'}`}>
+        <div className="flex justify-between items-center text-2xl font-bold">
+          <div>{title}</div>
+          <button
+            onClick={onClose}
+          >
+            &times;
+          </button>
+        </div>
         <div style={{whiteSpace: 'pre-line'}}>
           {children}
         </div>
