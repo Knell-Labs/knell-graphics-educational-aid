39f8b34847c67b07f4d35632a573c1199ac98a13
Alfredo <icealfredo123@gmail.com>
2023-08-31 16:31:52 -0500

    small changes

--- components/ui/left-panel/leftPanel.tsx
+++ components/ui/left-panel/leftPanel.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 
 export function LeftPanel(props){
   return (
-      <div className="fixed top-50 left-5 h-5/6 w-1/6 bg-customGray rounded-lg">
-      Curr Scene
-      </div>
+    <div className="fixed top-50 left-5 h-5/6 w-1/7 bg-customGray rounded-lg">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+        My Button
+      </button>
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+        My Button
+      </button>
+    </div>
   );
 };
