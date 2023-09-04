2e8b170dddbb8f4a53e80c3f161815bf4bb30019
c0mplikati0n <xavier6284@gmail.com>
2023-09-04 01:00:41 -0500

    Implementing in accordance to main, and other small changes

--- components/objects/Cube.tsx
+++ components/objects/Cube.tsx
@@ -1,23 +1,31 @@
 import { ThreeElements } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 
-export function CreateCube(props: ThreeElements['mesh']) {
+type CreateCubeProps = {
+  color?: string;
+  size?: [number, number, number];
+} & ThreeElements['mesh'];
+
+export function CreateCube({ color, size = [1, 1, 1], ...props }: CreateCubeProps) {
   const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  // If color prop is provided, use it; otherwise, use existing logic
+  const meshColor = color ? color : (hovered ? 'hotpink' : 'orange');
 
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      
+
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
 
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={meshColor} />
     </mesh>
   )
 }
