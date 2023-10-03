d057b5507e724563c2c86c8e8f9adebbeb0b729e
Alfredo <icealfredo123@gmail.com>
2023-10-03 12:54:45 -0500

    Directional light

--- components/objects/Lights.tsx
+++ components/objects/Lights.tsx
@@ -1,11 +1,8 @@
-import { ThreeElements, useFrame  } from '@react-three/fiber';
-import { useEffect, useRef, useState, useMemo } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState} from 'react';
 import * as THREE from 'three';
-import { useCursor  } from '@react-three/drei'
-import { Canvas, useThree } from '@react-three/fiber'
-import { useHelper } from '@react-three/drei';
 import { Plane } from '@react-three/drei';
-import {TransformCustomControls}  from "../../components/controls/objectControls/TransformCustomControls"
+import { TransformCustomControls }  from "../../components/controls/objectControls/TransformCustomControls"
 
 export function AmbientLightFunc(){
 
@@ -25,6 +22,7 @@ export function  DirectLightFunc(){
   const dirLight = useRef<THREE.DirectionalLight>(null);
   const planeRef = useRef<THREE.Mesh>(null);
   const lightHelperRef = useRef<THREE.DirectionalLightHelper | null>(null);
+  const [transformActive, setTransformActive] = useState<boolean>(false);
 
   useEffect(() => {
     if (dirLight.current) {
@@ -33,71 +31,31 @@ export function  DirectLightFunc(){
       dirLight.current.parent?.add(lightHelperRef.current);
     }
     
-    //return () => {
-    //  // Cleanup on component unmount
-    //  if (lightHelperRef.current) {
-    //    lightHelperRef.current.parent?.remove(lightHelperRef.current);
-    //    lightHelperRef.current.dispose();
-    //  }
-    //};
   }, []);
 
 
   useFrame(() => {
 
-    if (lightHelperRef.current && planeRef.current) {
-    //  //planeRef.matrixWorld
-    //  console.log(planeRef.current)
-    //  console.log(lightHelperRef.current.lightPlane);
-      //planeRef.current.matrixWorld = lightHelperRef.current.lightPlane.matrixWorld;
-    //  planeRef.current.matrix = lightHelperRef.current.lightPlane.matrix;
-    //  
-    //  // Example: Accessing the color property of the helper
-    //  //
-    //  //lightHelperRef.current.targetLine.visible = false
-    //  //lightHelperRef.current.lightPlane.visible = false
-    //  //console.log(lightHelperRef.current.targetLine);
-    //  let vertices = lightHelperRef.current.lightPlane.geometry.attributes.position.array
-    //  //console.log(vertices)
-    //  //for (let i = 0; i < vertices.length; i=i+3) {
-    //  ////      //a vertex' position is (vertices[i],vertices[i+1],vertices[i+2])
-    //  //      console.log(vertices[i],vertices[i+1],vertices[i+2])
-    //  //}
+    if(lightHelperRef.current && planeRef.current) {
+        planeRef.current.matrixWorld = lightHelperRef.current.lightPlane.matrixWorld;
     }
-
-
-
-    //console.log(lightHelperRef);
-    //if (cubeRef.current && outlineRef.current) {
-    //  outlineRef.current.position.copy(cubeRef.current.position);
-    //  outlineRef.current.rotation.copy(cubeRef.current.rotation);
-    //  outlineRef.current.scale.copy(cubeRef.current.scale);
-    //}
   });
 
   return (
     <>
-
       <Plane
-        ref={planeRef}
         visible = {false}
-        position = {
-          [
-            dirLight.current?.position.x || 0,
-            dirLight.current?.position.y || 0,
-            dirLight.current?.position.z || 0
-          ]
-        }
-        rotation = {
-          [
-            dirLight.current?.rotation.x || 0,
-            dirLight.current?.rotation.y || 0,
-            dirLight.current?.rotation.z || 0
-          ]
-        }
+        ref={planeRef}
+        onClick= {() =>{
+            setTransformActive(true) 
+        }}
+        onPointerMissed = { () => {
+            setTransformActive(false) 
+        }}
+
       />
 
-      <TransformCustomControls mesh = { planeRef }/>
+      {transformActive && <TransformCustomControls mesh = { dirLight }/>}
 
       <directionalLight 
         onClick={()=>{
