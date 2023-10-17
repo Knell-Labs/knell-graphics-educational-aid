aef6f6dbd8d72e42ff359da66a99da169e93d5f5
c0mplikati0n <xavier6284@gmail.com>
2023-10-16 23:44:43 -0500

    basic implementations

---
+++ components/objects/Hemisphere.tsx
@@ -0,0 +1,62 @@
+import { ThreeElements, useFrame } from '@react-three/fiber';
+import { useRef, useState, useMemo } from 'react';
+import * as THREE from 'three';
+import { useCursor } from '@react-three/drei'
+import { TransformCustomControls } from "../../components/controls/objectControls/TransformCustomControls"
+
+type CreateHemisphereProps = {
+  isObjectButtonPressed: boolean;
+  color?: string;
+  radius?: number;
+} & ThreeElements['mesh'];
+
+export function CreateHemisphere({ isObjectButtonPressed, color, radius = 1, ...props }: CreateHemisphereProps) {
+  const hemisphereRef = useRef<THREE.Mesh>(null!);
+  const outlineRef = useRef<THREE.LineSegments>(null!);
+
+  const [hovered, hover] = useState(false);
+  const [transformActive, setTransformActive] = useState(false);
+  const meshColor = color ? color : (transformActive ? 'orange' : 'white');
+  const groupRef = useRef<THREE.Group>(null);
+  const lineMaterial = useMemo(() => new THREE.LineBasicMaterial({ color: 0x000000, depthTest: true, opacity: 0.5, transparent: true }), []);
+
+  useCursor(hovered)
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.type = "HemisphereGroup"
+    }
+    if (hemisphereRef.current && outlineRef.current) {
+      outlineRef.current.position.copy(hemisphereRef.current.position);
+      outlineRef.current.rotation.copy(hemisphereRef.current.rotation);
+      outlineRef.current.scale.copy(hemisphereRef.current.scale);
+    }
+  });
+
+  return (
+    <group ref = { groupRef }>
+      <mesh
+        {...props}
+        ref = { hemisphereRef }
+
+        onClick = { (event) => {
+          if (!isObjectButtonPressed) {
+            (event.stopPropagation(), setTransformActive(true))
+          }
+        } }
+        onPointerMissed = { (event) => event.type === 'click' && setTransformActive(false) }
+        onPointerOver   = { (event) => (event.stopPropagation(), hover(true)) }
+        onPointerOut    = { (event) => hover(false) }
+      >
+        <sphereGeometry args = { [radius, 32, 32, 0, Math.PI] } />
+        <meshStandardMaterial color = { meshColor } />
+      </mesh>
+
+      {transformActive && <TransformCustomControls mesh = { hemisphereRef } />}
+
+      <lineSegments ref = { outlineRef } material = { lineMaterial }>
+        <edgesGeometry attach = "geometry" args = { [new THREE.SphereGeometry(radius, 32, 32, 0, Math.PI)] } />
+      </lineSegments>
+    </group>
+  )
+}
