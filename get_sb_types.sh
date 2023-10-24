cc5cabc8507679dab581a103e9474a93ad925173
CRAKZOR <nj4u26@aol.com>
2023-10-24 13:04:04 -0500

    db types script

--- get_sb_types.sh
+++ get_sb_types.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source .env.local
-echo $SUPABASE_PROJECT_REF
+
 echo $SUPABASE_ACCESS_TOKEN | npx supabase login
+
 npx supabase gen types typescript --project-id $SUPABASE_PROJECT_REF > ./src/supabase/database.types.ts
