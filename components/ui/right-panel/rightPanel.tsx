e56684a733246c80cbe57444a6ee6c6dd3f84398
Alfredo <icealfredo123@gmail.com>
2023-10-24 14:26:43 -0500

    more info

--- components/ui/right-panel/rightPanel.tsx
+++ components/ui/right-panel/rightPanel.tsx
@@ -7,9 +7,20 @@ interface RightPanelProps {
 export function RightPanel({objectClicked}: RightPanelProps) {
   useEffect( () => {
     if(objectClicked){
-        console.log(objectClicked?.position);
-        console.log(objectClicked?.rotation);
-        console.log(objectClicked?.quaternion);
+        objectClicked.geometry.computeBoundingSphere()
+        console.log(objectClicked);
+        console.log(objectClicked.geometry.type);//string
+        
+
+        /*The logs below should be things we should display and be able to edit*/
+        console.log(objectClicked.name);//string
+        console.log(objectClicked.material.color);//string
+        console.log(objectClicked.material.wireframe);//T or F
+        console.log(objectClicked?.position);//vector
+        console.log(objectClicked?.rotation);//vector
+        console.log(objectClicked?.scale);//vector
+        console.log(objectClicked?.quaternion);//vector
+
     }
 
   }, [objectClicked])
