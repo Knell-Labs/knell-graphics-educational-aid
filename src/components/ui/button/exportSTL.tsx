372a5393bf2ce1e449a870a909e1221e7613dc84
c0mplikati0n <xavier6284@gmail.com>
2023-11-28 04:34:48 -0600

    condensed version

--- src/components/ui/button/exportSTL.tsx
+++ src/components/ui/button/exportSTL.tsx
@@ -6,22 +6,16 @@ class CustomSTLExporter {
         let output = "solid exported\n";
 
         scene.traverse(function (object) {
-            //console.log(`Object Type: ${object.type}, Name: ${object.name}`);
     
             // Skip specific object types and names
-            if (['AmbientLight', 'DirectionalLight', 'PerspectiveCamera', 'GridHelper', 'Group', 'Line', 'Line2', 'DirLightGroup', 'DirectionalLightHelper'].includes(object.type) ||
-                ['AmbientLight', 'DirectionalLight', 'init-grid', 'grid-plane-hidden-helper', 'AxesHelperExclude', 'axes-helper', 'DirLightGroup', 'DirectionalLightHelper', 'ExcludeDirLight'].includes(object.name)) {
-                //console.log(`Skipping: ${object.type}, Name: ${object.name}`);
+            if (['Line2'].includes(object.type) || ['grid-plane-hidden-helper'].includes(object.name)) {
                 return;
             }
 
+            // Skips Directional Light Mesh
             if (object.parent && object.parent.type === 'DirLightGroup') {
-                console.log(`Skipping Mesh in DirLightGroup: `, object);
                 return;
             }
-    
-            // Log non-skipped objects
-            console.log(`Processing: ${object.type}, Name: ${object.name}`);
 
             if (object instanceof THREE.Mesh) {
                 let geometry = object.geometry; // Changed from const to let
